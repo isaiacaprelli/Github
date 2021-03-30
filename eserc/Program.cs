03f23f0fd17bbc2f7b13cfe83d60623a823cecdb
Isaia <isaia.caprelli.stud@ispascalcomandini.it>
2021-03-30 15:10:50 +0200

    first commit

---
+++ eserc/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace eserc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
